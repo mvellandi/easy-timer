682ce1c497ee6442e798349ff5e66e63aa7892c7
Mario Vellandi <mvellandi@hotmail.com>
2020-08-17 13:58:55 +0200

    updated mix deps

--- mix.exs
+++ mix.exs
@@ -33,12 +33,12 @@ defmodule EasyTimer.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.5.0", override: true},
-      {:phoenix_live_view, "~> 0.12.0"},
-      {:floki, ">= 0.0.0", only: :test},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix, "~> 1.5.4", override: true},
+      {:phoenix_live_view, "~> 0.14.4"},
+      {:floki, "~> 0.27.0"},
+      {:phoenix_html, "~> 2.14"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_dashboard, "~> 0.2.0"},
+      {:phoenix_live_dashboard, "~> 0.2.7"},
       {:telemetry_metrics, "~> 0.4"},
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
